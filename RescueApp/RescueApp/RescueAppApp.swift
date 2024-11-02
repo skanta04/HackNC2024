d52ea347af7fe228003fb639f1e0b821694ecf65
Sruthy Mammen <srumamm@unc.edu>
2024-11-02 17:33:34 -0400

    idk

--- RescueApp/RescueApp/RescueAppApp.swift
+++ RescueApp/RescueApp/RescueAppApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct RescueAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HistoryView()
         }
         .modelContainer(for: Message.self)
     }
