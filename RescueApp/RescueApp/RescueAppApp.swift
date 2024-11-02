cc752b64c7f67bcdf2f90132f273d2a2c6a86f7a
saishreeya@gmail.com <skanta@ad.unc.edu>
2024-11-02 11:21:53 -0400

    starter

---
+++ RescueApp/RescueApp/RescueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RescueAppApp.swift
+//  RescueApp
+//
+//  Created by Saishreeya Kantamsetty on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RescueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
