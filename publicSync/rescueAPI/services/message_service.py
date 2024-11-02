cb1a72a7d87080081d7b572df3b9875db88a0031
upasanal <111466862+upasanal@users.noreply.github.com>
2024-11-02 18:50:59 -0400

    adding update status functionality

--- publicSync/rescueAPI/services/message_service.py
+++ publicSync/rescueAPI/services/message_service.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from entities.message import Message
 from models.message_create import MessageCreate
 from typing import List
+from models.message_create import MessageStatus
 
 class MessageService:
     def create_message(self, message_data: MessageCreate, db: Session) -> Message:
@@ -21,3 +22,13 @@ class MessageService:
             db.commit()
             return True
         return False
+    
+    def update_message_status(self, message_id: int, status: MessageStatus, db: Session) -> Message:
+        db_message = db.query(Message).filter(Message.id == message_id).first()
+        if db_message:
+            db_message.status = status
+            db.commit()
+            db.refresh(db_message)
+            return db_message
+        return None
+
